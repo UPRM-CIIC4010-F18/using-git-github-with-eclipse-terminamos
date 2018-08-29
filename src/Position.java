82453a70d6f2e0134119e2e95220b20b17624546
JanPaulCulpeper <jan.culpeper1@upr.edu>
2018-08-29 11:08:48 -0400

    pregunta3

--- src/Position.java
+++ src/Position.java
@@ -69,7 +69,16 @@ public class Position {
 		
 		//Add Code Here [You have to use If/Else to get graded]
 		
-		return 0; //Temporal Return
+		double totalYearly;
+		if(jobType == true){
+			
+			totalYearly =  hSalary*40*4.5*12;
+			return totalYearly;
+		}
+		else{
+			totalYearly =  hSalary*20*4.5*12;
+return totalYearly;
 		
 	}
+}
 }
